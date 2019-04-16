7d9676b5a5b4237b349c9f94c4f62e6975d2f59d
Caixa <gabriel.ferreira@castgroup.com.br>
2019-04-16 18:05:48 -0300

    Incluindo dashboard no menu

--- angmaterial/src/app/app-routing.module.ts
+++ angmaterial/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { TerceiraPaginaComponent } from './terceira-pagina/terceira-pagina.component';
+import { MinhaDashboardComponent } from './minha-dashboard/minha-dashboard.component';
 
 const routes: Routes = [
+  { path: '', component: MinhaDashboardComponent },
   { path: 'primeira-pagina', component: PrimeiraPaginaComponent },
   { path: 'segunda-pagina', component: SegundaPaginaComponent },
   { path: 'terceira-pagina', component: TerceiraPaginaComponent }
